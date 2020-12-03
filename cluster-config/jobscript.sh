46d7d2c255f2308c85610aa078cdd091d7f32f1b
Jesse Engreitz <engreitz@stanford.edu>
2020-12-03 11:55:40 -0800

    adjusting to use conda environment

--- cluster-config/jobscript.sh
+++ cluster-config/jobscript.sh
@@ -1,15 +1,7 @@
 #!/bin/sh
 # properties = {properties}
 
-source /broad/software/scripts/useuse
-reuse -q .python-3.5.1
-source /seq/lincRNA/Ben/VENV_BEN_UMITOOLS/bin/activate
-reuse -q BEDTools
-reuse -q Bowtie
-reuse -q Samtools
-reuse -q FASTX-Toolkit
-reuse BLAST
-# reuse Matlab
-reuse .matlab-2017a
+## Add any needed cluster setup commands here
+conda activate hyprseq
 
 {exec_job}
